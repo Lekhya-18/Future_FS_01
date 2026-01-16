9318cb5831ef55826e24461fdbd4ab5d2489c823
Lekhya-18 <pittalekhya.24.cse@anits.edu.in>
2026-01-16 14:38:58 +0530

    My Portfolio

--- my-react-app/src/components/Contact.jsx
+++ my-react-app/src/components/Contact.jsx
@@ -1,7 +1,28 @@
+import email from "../assets/email.png"
+import github from "../assets/github.png"
+import linkedin from "../assets/linkedin.png"
 export default function Contact() {
   return (
     <section id="contact">
       <h2>Contact Me</h2>
+
+      <div className="contact-links">
+        <a href="mailto:lekhyapitta@gmail.com">
+          <img src={email} alt="Email" />
+          <span>Email</span>
+        </a>
+
+        <a href="https://www.linkedin.com/in/lekhya-p-103274345" target="_blank">
+          <img src={linkedin} alt="LinkedIn" />
+          <span>LinkedIn</span>
+        </a>
+
+        <a href="https://github.com/Lekhya-18" target="_blank">
+          <img src={github} alt="GitHub" />
+          <span>GitHub</span>
+        </a>
+      </div>
+
       <form>
         <input type="text" placeholder="Name" />
         <input type="email" placeholder="Email" />
@@ -9,5 +30,5 @@ export default function Contact() {
         <button>Send</button>
       </form>
     </section>
-  );
+  )
 }
